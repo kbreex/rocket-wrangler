747f6349033806605edae52de002a4da2f3f104f
Scravers <jamestephen@shaw.ca>
2024-06-15 12:34:00 -0600

    Added a asteroid prefab and an obsticale spawn script

---
+++ Assets/Scripts/ObstacleSpawnScript.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObsticalSpawnScript : MonoBehaviour
+{
+
+
+
+    private float timer;
+
+    public float spawnRate;
+
+    public GameObject asteroid;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set timer to 0 and the spawn rate and spawn the first asteroid
+        timer = 0;
+        spawnRate = 10;
+
+        SpawnAsteroid();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer < spawnRate){
+            // Incament the timer
+            timer += Time.deltaTime;
+        }
+        else{
+            // Spawn the next asteroid and set the time to 0
+            SpawnAsteroid();
+            timer = 0;
+        }
+
+    }
+
+    void SpawnAsteroid(){
+
+        Instantiate(asteroid, new Vector3(0, 7, 0), transform.rotation);
+
+    }
+}
